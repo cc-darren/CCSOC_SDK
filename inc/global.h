50e91500742a5b648869c55c2a8056d3004469f9
pattaya.tsao <pattaya.tsao@cloudchip.com>
2017-03-14 14:02:56 +0800

    add global.h

---
+++ inc/global.h
@@ -0,0 +1,54 @@
+/******************************************************************************
+*  Copyright 2017, Cloud Chip, Inc.
+*  ---------------------------------------------------------------------------
+*  Statement:
+*  ----------
+*  This software is protected by Copyright and the information contained
+*  herein is confidential. The software may not be copied and the information
+*  contained herein may not be used or disclosed except with the written
+*  permission of Cloud Chip, Inc. (C) 2017
+******************************************************************************/
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include "ll.h"
+#include "compiler.h"
+
+
+#define FALSE               (0)
+#define TRUE                (1)
+#define SUCCESS             TRUE
+#define FAIL                FALSE
+
+
+typedef unsigned int        UINT32;
+typedef unsigned short      UINT16;
+typedef unsigned char       UINT8;
+typedef signed int          INT32;
+typedef signed short        INT16;
+typedef signed char         INT8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //_GLOBAL_H_
