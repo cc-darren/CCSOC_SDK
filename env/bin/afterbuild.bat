6f6130d8c60244396a1e55469e07abfca6e5f1cb
pattaya.tsao <pattaya.tsao@cloudchip.com>
2017-11-09 11:01:06 +0800

    Auto generate xxx.HEX1b and xxx.HEX4b for simulation in BSP projects

---
+++ env/bin/afterbuild.bat
@@ -0,0 +1,3 @@
+C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --bincombined --output .\Objects\%1.bin %2
+C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --vhx --8x1 --output .\Objects\%1.HEX1b %2
+C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --vhx --32x1 --output .\Objects\%1.HEX4b %2
