9e4e794c8aa0ab41fa2fb2f6cc242c3adb9be377
blake.lai <blake.lai@cloudchip.com>
2017-12-08 18:49:21 +0800

    MCP3421 export ADC interface

--- module/adc/adc_mcp3421.h
+++ module/adc/adc_mcp3421.h
@@ -8,14 +8,29 @@
 
 /* Exported macro ------------------------------------------------------------*/
 //ADC sensor interface
-#define ADC_Init()
-#define ADC_ReadRaw(val)
-#define ADC_ReadScales(val)
-#define ADC_ReadSampleRate(rate)
-#define ADC_WritePga(pga)
-#define ADC_WriteSampleRate(rate)
+#define ADC_Init()                             MCP3421Init()
+#define ADC_ReadRaw(val)                       MCP3421ReadRaw(val)
+#define ADC_ReadScales(val)                    MCP3421ReadSampleRate(val)
+#define ADC_ReadSampleRate(rate)               MCP3421ReadSacle(rate)
+#define ADC_WritePga(pga)                      MCP3421WritePga(pga)
+#define ADC_WriteSampleRate(rate)              MCP3421WriteSampleFreq(rate)
 
 
 /* Exported constants --------------------------------------------------------*/
+
+
+
+
+
+
+
+int MCP3421Init(void);
+int MCP3421ReadRaw(int *piVal);
+int MCP3421ReadSacle(int *piVal);
+int MCP3421ReadSampleRate(int *piVal);
+int MCP3421WritePga(int iVal);
+int MCP3421WriteSampleFreq(int iVal);
+
+
 
 #endif //_ADC_NULL_DRIVER_H
