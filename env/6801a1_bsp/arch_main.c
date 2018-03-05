054b0b1d03f2b222a92027841eea49125eb571ca
blake.lai <blake.lai@cloudchip.com>
2018-03-05 11:31:38 +0800

    - Fix 6801a1 bsp project build fail

---
+++ env/6801a1_bsp/arch_main.c
@@ -0,0 +1,53 @@
+/* Copyright (c) 2018 Cloudchip, Inc. All Rights Reserved.
+ *
+ * The information contained herein is property of Cloudchip, Inc.
+ * Terms and conditions of usage are described in detail in CLOUDCHIP
+ * STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information.
+ * NO WARRANTY of ANY KIND is provided. This heading must NOT be removed 
+ * from the file.
+ */
+
+/*
+ * INCLUDES
+ ****************************************************************************************
+ */
+#include <string.h>
+#include "ll.h"
+#include "drvi_init.h"
+#include "tracer.h"
+#include "project.h"
+
+uint8_t g_GyroEnable = 1;
+
+extern void sys_InitMain(void);
+
+/*
+ * MAIN FUNCTION
+ ****************************************************************************************
+ */
+
+int main(void)
+{
+    //Must be first in main()
+    sys_InitMain();
+
+    //start interrupt handling
+    GLOBAL_INT_START(); 
+
+    drvi_initialize();
+
+    TracerInfo("== CC6801 Start ==\r\n");
+
+    /*******************************/
+    /****** Application Start ******/
+    /*******************************/
+
+    while(1)
+    {
+
+    }
+    //return(0);
+}
+
