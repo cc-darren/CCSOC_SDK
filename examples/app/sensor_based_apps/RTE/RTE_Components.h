1ec079bddaaa66153d75878a02c7cdb7c2275111
louis.li <louis.li@cloudchip.com>
2018-04-09 15:55:16 +0800

    [NEW FEATURE] example: sensor based applications, are supported

---
+++ examples/app/sensor_based_apps/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cc6801' 
+ * Target:  'EVB_M0p' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
