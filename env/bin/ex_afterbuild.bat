b18e71677e2e638c50ddf75cb3d8ab09406f6a10
pattaya.tsao <pattaya.tsao@cloudchip.com>
2018-12-03 18:46:57 +0800

    add Flash programming setting and new ex_afterbuild.bat

---
+++ env/bin/ex_afterbuild.bat
@@ -0,0 +1,6 @@
+C:\Keil_v5\ARM\ARMCC\Bin\fromelf.exe --i32combined --output .\Release\%1.hex   %2
+C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --bincombined --output .\Release\%1.bin   %2
+C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --vhx --8x1   --output .\Release\%1.HEX1b %2
+C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --vhx --32x1  --output .\Release\%1.HEX4b %2
+..\..\..\env\bin\CC_Hex2Bin.exe .\Release\%1 .\Release\enc\%1
+
