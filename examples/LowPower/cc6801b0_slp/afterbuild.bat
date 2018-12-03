67b8508d19b4e2a9387be3c82510c2b839e4e733
pattaya.tsao <pattaya.tsao@cloudchip.com>
2018-12-03 17:09:03 +0800

    remove ble/lowpower/app which are old A0 project and remove 'b0' from project name

--- examples/LowPower/cc6801b0_slp/afterbuild.bat
+++
@@ -1,3 +0,0 @@
-C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --bincombined --output .\Objects\%1.bin %2
-C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --vhx --8x1 --output .\Objects\%1.HEX1b %2
-C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe --vhx --32x1 --output .\Objects\%1.HEX4b %2
