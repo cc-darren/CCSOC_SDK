0292a9476d069f54b94a2526b7b8ff073d4b89ab
pattaya.tsao <pattaya.tsao@cloudchip.com>
2019-01-25 18:58:26 +0800

    Change CC_Hex2Bin.exe path in afterbuild.bat for new folder architecture. All projects can not call CC_Hex2Bin temporarily

--- env/bin/afterbuild.bat
+++ env/bin/afterbuild.bat
@@ -2,5 +2,5 @@
 %3ARM\ARMCC\bin\fromelf.exe --bincombined --output .\Release\%1.bin   %2
 %3ARM\ARMCC\bin\fromelf.exe --vhx --8x1   --output .\Release\%1.HEX1b %2
 %3ARM\ARMCC\bin\fromelf.exe --vhx --32x1  --output .\Release\%1.HEX4b %2
-..\..\..\env\bin\CC_Hex2Bin.exe .\Release\%1 .\Release\enc\%1
+..\..\..\..\..\env\bin\CC_Hex2Bin.exe .\Release\%1 .\Release\enc\%1
 
